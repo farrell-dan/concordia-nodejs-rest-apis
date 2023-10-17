f8c05229d872a29b50a4243713742771f9342f8f
farrell-dan <140118556+farrell-dan@users.noreply.github.com>
2023-10-16 21:05:37 -0400

    Exercise 2

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 
 const express = require('express');
 const morgan = require('morgan');
+const{clients} = require('./data/clients');
 
 express()
   .use(function (req, res, next) {
@@ -18,5 +19,40 @@ express()
   .use(express.urlencoded({ extended: false }))
 
   // endpoints
+
+  .get(`/clients`, (req, res) => {
+    res.status(200).json(clients)
+  })
+
+  .get('/clients/:clientId', (req, res) => {
+  const clientId = req.params.clientId;
+  const client = clients.find((index) => index.id === clientId);
+    if (!client) {
+      return res.status(404).json({ error: 'Client not found' });
+  }
+  res.status(200).json(client);
+})
+
+.post('/clients/:clientId/email', (req, res) => {
+  const clientId = req.params.clientId;
+  const { email } = req.body;
+  const client = clients.find((index) => index.id === clientId);
+  if (!client) {
+    return res.status(404).json({ error: 'Client not found' });
+  }
+  client.email = email;
+  res.status(200).json(client);
+})
+
+.delete('/clients/:clientId', (req, res) => {
+  const clientId = req.params.clientId;
+  const index = clients.findIndex((index) => index.id === clientId);
+  if (index === -1) {
+    return res.status(404).json({ error: 'Client not found' });
+  }
+  clients.splice(index, 1);
+  res.status(204).send();
+})
+
 
   .listen(8000, () => console.log(`Listening on port 8000`));
